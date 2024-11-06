5e23d7df2d03b85e133edd3c66c4637e60dc2324
Michał Zarzycki <80000683+Assassin-PL@users.noreply.github.com>
2024-11-06 19:12:14 +0100

    Lakecja cz1

--- Klasy.py
+++ Klasy.py
@@ -2,19 +2,3 @@ import pygame
 import random
 import configparser
 
-class Jablko(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        # Inicjalizacja configparser
-        config = configparser.ConfigParser()
-        config.read('config.cfg')
-
-        # Wczytywanie wartości jako stałe
-        self.FPS = int(config['SETTINGS']['FPS'])
-        self.WIELKOSC_KAFLA = int(config['SETTINGS']['WIELKOSC_KAFLA'])
-        self.SZEROKOSC_EKRANU = int(config['SETTINGS']['SZEROKOSC_EKRANU'])
-        self.WYSOKOSC_EKRANU = int(config['SETTINGS']['WYSOKOSC_EKRANU'])
-
-        super(Jablko, self).__init__()
-        self.obraz = pygame.image.load("images/apple.png")
-        self.rect = pygame.Rect(random.randint(0, 24) * self.WIELKOSC_KAFLA, random.randint(0, 18) * self.WIELKOSC_KAFLA, self.WIELKOSC_KAFLA, self.WIELKOSC_KAFLA)
--- main.py
+++ main.py
@@ -2,60 +2,25 @@ import pygame
 import random
 import time
 import configparser
-from Klasy import Jablko
+# from Klasy import Jablko
 
-# Inicjalizacja configparser
-config = configparser.ConfigParser()
-config.read('config.cfg')
+FPS = 60
+WIELKOSC_KAFLA = 32
+SZEROKOSC_EKRANU = 800
+WYSOKOSC_EKRANU = 608
 
-# Wczytywanie wartości jako stałe
-FPS = int(config['SETTINGS']['FPS'])
-WIELKOSC_KAFLA = int(config['SETTINGS']['WIELKOSC_KAFLA'])
-SZEROKOSC_EKRANU = int(config['SETTINGS']['SZEROKOSC_EKRANU'])
-WYSOKOSC_EKRANU = int(config['SETTINGS']['WYSOKOSC_EKRANU'])
+wiersz = int(SZEROKOSC_EKRANU / WIELKOSC_KAFLA)
+kolumna = int(WYSOKOSC_EKRANU/WIELKOSC_KAFLA)
 
+#stworznie tlo
 tlo = pygame.Surface((SZEROKOSC_EKRANU,WYSOKOSC_EKRANU))
-
-wiersz : int= int(SZEROKOSC_EKRANU / WIELKOSC_KAFLA)
-kolumna : int = int(WYSOKOSC_EKRANU / WIELKOSC_KAFLA)
 
 for i in range(wiersz):
     for j in range(kolumna):
-        obraz = pygame.image.load("images/background.png")
-        maska = (random.randrange(0, 20), random.randrange(0, 20), random.randrange(0,20))
-        obraz.fill(maska, special_flags=pygame.BLEND_ADD)
-        tlo.blit(obraz, (i * WIELKOSC_KAFLA, j * WIELKOSC_KAFLA))
+        obrazek = pygame.image.load("images/background.png")
+        maska = (random.randrange(0,20), random.randrange(0,20),
+                 random.randrange(0,20))
+        obrazek.fill(maska, special_flags=pygame.BLEND_ADD)
 
 #ustawienia
 pygame.init()
-#obiekt ekranu i zegara
-ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
-zegar = pygame.time.Clock()
-
-#jabłka
-jablko = Jablko()
-if isinstance(jablko, pygame.sprite.Sprite):
-    jablka = pygame.sprite.Group()
-    jablka.add(jablko)
-
-gra_dziala :bool=True
-while gra_dziala :
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                gra_dziala = False
-        elif event.type == pygame.QUIT:
-            gra_dziala = False
-        #rysowanie tla
-        ekran.blit(tlo, (0, 0))
-        # rysowanie jablek
-        for jablko in jablka:
-            ekran.blit(jablko.obraz, jablko.rect)
-        #czyszczenie ekranu
-        pygame.display.flip()
-        #wygaszanie ekranu
-        zegar.tick(FPS)
-
-time.sleep(3)
-pygame.quit()
-
