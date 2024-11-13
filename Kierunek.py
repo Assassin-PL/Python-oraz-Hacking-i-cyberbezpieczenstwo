1de8744295f66f20d1c6e31c05b876607762ace8
Michał Zarzycki <michael.za1995@gmail.com>
2024-11-13 19:12:56 +0100

    Utowrzony Klase Enum i Waz

--- Kierunek.py
+++ Kierunek.py
@@ -1,2 +1,8 @@
 from enum import Enum
 #Tutaj stworzymy klase z kierunkiem naszego weza
+
+class Kierunek(Enum):
+    GORA  = 0
+    PRAWO = 1
+    DOL = 2
+    LEWO = 3
--- Waz.py
+++ Waz.py
@@ -1,4 +1,26 @@
 import pygame
-from Kierunek import *
+from pygame.sprite import AbstractGroup
+from Kierunek import Kierunek
+import configparser
 
 #Tutaj stowrzymy klase weza
+
+configpath=("config.cfg")
+configSnake = configparser.ConfigParser()
+configSnake.read(configpath)
+
+WIELKOSC_KAFLA :int = int (configSnake['SETTINGS']['WIELKOSC_KAFLA'])
+
+class Waz(pygame.sprite.Sprite):
+    orginalny_obraz = None
+    obraz = None
+    rect = None
+    
+    def __init__(self):
+        #orginalny obraz glowy
+        self.orginalny_obraz = pygame.image.load("images/head.png")
+        #nasz obraz pomocniczy
+        self.obraz = pygame.transform.rotate(self.orginalny_obraz , 0)
+        #wspolrzedne glowy 
+        self.rect = self.obraz.get_rect(center=(12*WIELKOSC_KAFLA+16 , 9*WIELKOSC_KAFLA + 16))
+         
