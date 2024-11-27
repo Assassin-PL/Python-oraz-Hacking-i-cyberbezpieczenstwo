518e152624b44a8a0d1fa2b98f335f280147decd
Michał Zarzycki <michael.za1995@gmail.com>
2024-11-27 19:25:41 +0100

    poprawki

--- platforma.py
+++ platforma.py
@@ -5,3 +5,11 @@ from utlis import Config
 config = Config()
 ustawienia : str = "SETTINGS"
 sciezki : str = "PATHS"
+
+class Platforma(pygame.sprite.Sprite):
+    def __init__(self, x, y, szerokosc, wysokosc):
+        super().__init__()
+        self.config = Config()
+        self.ustawienia : str = "SETTINGS"
+        self.sciezki : str = "PATHS"
+        self.obraz = pygame.image.load(self.config.get(self.sciezki, "platforma")) 
