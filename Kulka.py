71e8dd25aae5132c996f9946b17d6a5629774f40
Michał Zarzycki <michael.za1995@gmail.com>
2024-12-04 19:13:01 +0100

    do przerwy

--- Kulka.py
+++ Kulka.py
@@ -7,3 +7,15 @@ ustawienia : str = "SETTINGS"
 sciezki : str = "PATHS"
 SZEROKOSC_EKRANU = int(config.get(ustawienia,"SZEROKOSC_EKRANU"))
 WYSOKOSC_EKRANU = int(config.get(ustawienia, "WYSOKOSC_EKRANU"))
+
+vec = pygame.math.Vector2
+class Kulka(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Kulka, self).__init__()
+        self.config = Config()
+        self.ustawienia : str = "SETTINGS"
+        self.sciezki : str = "PATHS"
+        self.obraz = pygame.image.load(self.config.get(self.sciezki, "ball")) 
+        self.zresetujobraz()
+
+    
--- platforma.py
+++ platforma.py
@@ -15,10 +15,15 @@ class Platforma(pygame.sprite.Sprite):
         self.sciezki : str = "PATHS"
         self.obraz = pygame.image.load(self.config.get(self.sciezki, "pad")) 
         self.zresetujobraz()
+        self.porusza_sie  = 0
     
     def zresetujobraz(self):
         self.pozycja = pygame.Rect(SZEROKOSC_EKRANU / 2 -70 , WYSOKOSC_EKRANU - 100, 140, 30)
 
     def ruszaj_platforma(self, wartosc):
         predkosc = int(self.config.get_int(self.ustawienia, "PREDKOSC"))
+        self.porusza_sie = wartosc
         self.pozycja.move_ip(wartosc * predkosc, 0)
+    
+    def aktualizuj(self):
+        self.porusza_sie = 0
