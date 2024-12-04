c1dc2e4a33f9e5aab0075b3ea0afee2710e5fbf2
Michał Zarzycki <michael.za1995@gmail.com>
2024-12-04 20:02:29 +0100

    koniec zajec

--- Kulka.py
+++ Kulka.py
@@ -32,4 +32,20 @@ class Kulka(pygame.sprite.Sprite):
     def aktualizuj(self, platforma):
         self.wspolrzedne += self.wektor
         self.pozycja.center = self.wspolrzedne
+        self.sprawdz_pozcje(platforma)
     
+    def sprawdz_pozcje(self, platforma):
+        if self.pozycja.x <=0:
+            self.wektor.x *= -1
+        if self.pozycja.right >= SZEROKOSC_EKRANU:
+            self.wektor.x *= -1
+        if self.pozycja.top <= 0:
+            self.wektor.y *= -1
+        if self.pozycja.bottom >= WYSOKOSC_EKRANU:
+            self.przegrana = True
+            
+        if self.pozycja.colliderect(platforma.pozycja):
+            self.wektor.y *= -1
+            self.wektor.x += platforma.porusza_sie*5
+            if self.wektor.x < -10: self.wektor.x = -10
+            if self.wektor.x > 10: self.wektor.x = 10
