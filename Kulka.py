f0018abeb2a42a4de8182f31e190d8a55d55552e
Michał Zarzycki <michael.za1995@gmail.com>
2024-12-04 19:30:25 +0100

    Dodanie obiektu kulki

--- Kulka.py
+++ Kulka.py
@@ -1,5 +1,6 @@
 import pygame
 from utlis import Config
+import random
 
 #Klasa obslugujaca nasz plik konfiguracyjny
 config = Config()
@@ -15,7 +16,19 @@ class Kulka(pygame.sprite.Sprite):
         self.config = Config()
         self.ustawienia : str = "SETTINGS"
         self.sciezki : str = "PATHS"
-        self.obraz = pygame.image.load(self.config.get(self.sciezki, "ball")) 
-        self.zresetujobraz()
-
+        self.obraz = pygame.image.load(self.config.get(self.sciezki, "pilka")) 
+        self.zresetujpozycje()
+        self.u = 16
+        self.przegrana = False
+        
+    def zresetujpozycje(self):
+        self.wspolrzedne = vec(SZEROKOSC_EKRANU / 2, WYSOKOSC_EKRANU - 140)
+        self.pozycja = self.obraz.get_rect(center=self.wspolrzedne)
+        self.wektor = vec(0, -10)
+        self.kat_nachylenia = random.randrange(-30, 30)
+        self.wektor.rotate_ip(self.kat_nachylenia)
+    
+    def aktualizuj(self):
+        self.wspolrzedne += self.wektor
+        self.pozycja.center = self.wspolrzedne
     
