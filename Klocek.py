842ab4d88ea01233b7dad9252af61d43c0568146
Michał Zarzycki <michael.za1995@gmail.com>
2024-12-11 19:10:39 +0100

    przerobienie kodu do przerwy

--- Klocek.py
+++ Klocek.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 from utlis import Config
 
 #Klasa obslugujaca nasz plik konfiguracyjny
@@ -9,8 +10,22 @@ SZEROKOSC_EKRANU = int(config.get(ustawienia,"SZEROKOSC_EKRANU"))
 WYSOKOSC_EKRANU = int(config.get(ustawienia, "WYSOKOSC_EKRANU"))
 
 class Klocek(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x ,y ,zdrowie):
         super().__init__()
         self.config = Config()
         self.ustawienia : str = "SETTINGS"
         self.sciezki : str = "PATHS"
+        self.obraz_org = pygame.image.load(self.config.get(self.sciezki, "cegla"))
+        self.pozycja = pygame.Rect(x, y, 96, 48)
+        self.zdrowie = zdrowie
+    
+    def aktualizuj(self):
+        maska_koloru = 0
+        if self.zdrowie == 3:
+            maska_koloru = (128,0,0)
+        if self.zdrowie == 2:
+            maska_koloru = (0,0,128)
+        if self.zdrowie == 1:
+            maska_koloru = (0,128,0)
+        self.obraz = copy.copy(self.obraz_org)
+        self.obraz.fill(maska_koloru, special_flags=pygame.BLEND_ADD)
